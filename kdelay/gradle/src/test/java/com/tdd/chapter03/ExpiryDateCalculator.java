8c18782c429d69dddaeb5aeae67153f6e40641d5
김지연 <kdelay20@gmail.com>
2024-05-09 16:53:55 +0900

    refactor: #19 구현 리팩토링

--- kdelay/gradle/src/test/java/com/tdd/chapter03/ExpiryDateCalculator.java
+++ kdelay/gradle/src/test/java/com/tdd/chapter03/ExpiryDateCalculator.java
@@ -6,10 +6,12 @@ import java.time.YearMonth;
 public class ExpiryDateCalculator {
 
     /**
-     * calcualteExpiryDate 메서드 리팩토링 순서 정리
-     * 1. 중복 코드 제거
-     * 2. 만료일 계산을 위한 필요한 값이 3개로 늘어났으므로 파라미터 객체(PayData)로 변경
-     * 3. 상수 -> 변수 변경
+     * 리팩토링 순서 정리
+     * 1. Test 중복 코드 함수화 (assertExpiryDate)
+     * 2. 만료일 계산을 위한 필요한 값이 3개로 늘어났으므로 파라미터 객체로 변경 (PayData)
+     * 3. 상수 -> 변수 변경 (addedMonths)
+     * 4. 중복 코드 변수화 (dayLenOfCandiMon, dayOfFirstBilling)
+     * 5. 비즈니스 로직 함수화 (expiryDateUsingFirstBillingDate)
      */
     public LocalDate calculateExpiryDate(PayData payData) {
 
@@ -17,27 +19,33 @@ public class ExpiryDateCalculator {
 
         //첫 납부일이 있는 경우
         if (payData.getFirstBillingDate() != null) {
-            //금액만큼 만료 달이 증가된 임시 만료일
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-
-            //첫 납부일 != 임시 만료일
-            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
-
-                System.out.println("YearMonth.from(candidateExp) = " + YearMonth.from(candidateExp));
-                
-                //2만원 이상 납부 + (임시 만료일 < 첫 납부일): 임시 만료 달의 일자가 넘는 경우
-                // -> 임시 만료 달의 마지막 날짜를 만료일로 사용
-                if(YearMonth.from(candidateExp).lengthOfMonth() < payData.getFirstBillingDate().getDayOfMonth()) {
-                    return candidateExp.withDayOfMonth(
-                            YearMonth.from(candidateExp).lengthOfMonth()
-                    );
-                }
-                //1만원 납부 -> 첫 납부일을 만료일로 사용
-                return candidateExp.withDayOfMonth(
-                        payData.getFirstBillingDate().getDayOfMonth()
-                );
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
-        return payData.getBillingDate().plusMonths(addedMonths);
     }
+
+    private static LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
+        //금액만큼 만료 달이 증가된 임시 만료일
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+
+        //첫 납부일의 일자
+        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+
+        //첫 납부일 != 임시 만료일
+        if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
+
+            //만료 달의 가장 마지막 일자
+            final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+
+            //2만원 이상 납부 + (임시 만료일 < 첫 납부일): 임시 만료 달의 일자가 넘는 경우
+            // -> 임시 만료 달의 마지막 날짜를 만료일로 사용
+            if(dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            //1만원 납부 -> 첫 납부일을 만료일로 사용
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else return candidateExp; //첫 납부일 == 임시 만료일인 경우, 그대로 반환
+    }
+
 }
